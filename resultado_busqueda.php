f760553daac5cdd9231a45c78576b90026f3bf84
Jessica <jessyamaralfranco@gmail.com>
2019-05-18 02:37:19 -0500

    Búsqueda casi terminada
    Hace los querys correctamente. Faltan las vistas y agregar enlaces a cada receta.

--- resultado_busqueda.php
+++ resultado_busqueda.php
@@ -5,40 +5,106 @@
 include 'php/conexion.php';
 
 $busqueda = $_POST ['busqueda']; // receta nombre_receta 
-if(isset($tipo)){
+$tipo = $_POST ['tipo'];
+$sabor = $_POST["sabor"]; // from clasificacion where id_puntajes == value $sabor
+$dificultad = $_POST["dificultad"]; // id_puntajes
+$accesibilidad = $_POST["accesibilidad"]; // id_puntajes
+$tiempo = $_POST["tiempo"]; // id_puntajes
+$nacionalidad = $_POST["nacionalidad"]; // receta id_nacionalidad
+
+if(!empty($tipo)){
 $tipo = $_POST['tipo']; // receta id_categoria
 }else{
     $tipo = 0;
 }
 
-$sabor = $_POST["cat_sabor"]; // from clasificacion where id_puntajes == value $sabor
-$dificultad = $_POST["cat_dificultad"]; // id_puntajes
-$accesibilidad = $_POST["cat_accesibilidad"]; // id_puntajes
-$tiempo = $_POST["cat_tiempo"]; // id_puntajes
-$nacionalidad = $_POST["nacionalidad"]; // receta id_nacionalidad
+if(isset($sabor)){
+$sabor = $_POST['sabor']; // receta id_categoria
+}else{
+    $sabor = 0;
+}
 
-echo $busqueda."<br>";
-echo $tipo."<br>";
-echo $sabor."<br>";
-echo $dificultad."<br>";
-echo $accesibilidad."<br>";
-echo $tiempo."<br>";
-echo $nacionalidad."<br>";
 
-// if(!empty($busqueda) && empty($tipo) && empty($tipo) && empty($sabor) && empty($dificultad) 
-// && empty($tipo) ){
+// echo $busqueda."<br>";
+// echo $tipo."<br>";
+// echo $sabor."<br>";
+// echo $dificultad."<br>";
+// echo $accesibilidad."<br>";
+// echo $tiempo."<br>";
+// echo $nacionalidad."<br>";
 
-//     $query = "SELECT nombre_receta FROM receta WHERE nombre_receta LIKE '%$busqueda%'";
-//     $rs = mysqli_query ($conexion, $query);
-// }
+function is_empty ($query){
 
-// $row = mysqli_fetch_assoc($rs);
+    if (strlen($query)==39) {
+    return true;
+    }else {return false;}
+}
+
+$query="SELECT nombre_receta FROM receta WHERE ";
+
+echo strlen($query);
+
+if (!empty ($busqueda) && !is_empty ($query)){
+    $query.= " AND nombre_receta = %$busqueda%";
+}else if(!empty ($busqueda) && is_empty ($query)){
+    $query.= " nombre_receta LIKE '%$busqueda%'";
+}
+
+if (!empty ($tipo) && !is_empty ($query)){
+    $query.= " AND id_categorias=$tipo";
+}else if(!empty ($tipo) && is_empty ($query)){
+    $query.= "  id_categorias=$tipo";
+}
+
+if (!empty ($sabor) && !is_empty ($query)){
+    $query.= " AND sabor=$sabor";
+}else if(!empty ($sabor) && is_empty ($query)){
+    $query.= "  sabor=$sabor";
+}
+
+is_empty($query);
+
+if (!empty ($dificultad) && !is_empty ($query)){
+    $query.= " AND dificultad=$dificultad";
+}else if(!empty ($dificultad) && is_empty ($query)){
+    $query.= "  dificultad=$dificultad";
+}
+
+is_empty($query);
+
+if (!empty ($accesibilidad) && !is_empty ($query)){
+    $query.= " AND accesibilidad=$accesibilidad";
+}else if(!empty ($accesibilidad) && is_empty ($query)){
+    $query.= "  accesibilidad=$accesibilidad";
+}
+
+is_empty($query);
+
+if (!empty ($tiempo) && !is_empty ($query)){
+    $query.= " AND tiempo=$tiempo";
+}else if(!empty ($tiempo) && is_empty ($query)){
+    $query.= "  tiempo=$tiempo";
+}
+
+is_empty($query);
+
+if (!empty ($nacionalidad) && !is_empty ($query)){
+    $query.= " AND id_nacionalidad=$nacionalidad";
+}else if(!empty ($nacionalidad) && is_empty ($query)){
+    $query.= "  id_nacionalidad=$nacionalidad";
+}
+
+$rs = mysqli_query ($conexion, $query);
+echo '<br>'.$query;
 
 while(($row=mysqli_fetch_assoc($rs)))  { 
     echo "Nombre: ".($row['nombre_receta'])."<br>"; 
 }
 
 ?>
+
+
+
 
 </html>
 
