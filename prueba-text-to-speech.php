34d36c7b6838a85d34bef644d183f7a9951de90b
Jessica <jessyamaralfranco@gmail.com>
2019-05-28 14:31:51 -0500

    Dictado casi listo
    Dicta nombre y primer ingrediente al mismo tiempo gggg

--- prueba-text-to-speech.php
+++ prueba-text-to-speech.php
@@ -16,6 +16,8 @@ $client = new TextToSpeechClient();
 $query= "SELECT * FROM procedimiento WHERE id_receta = 17";
 $rs = mysqli_query ($conexion, $query);
 
+//GUARDA AUDIO PROCEDIMIENTO
+
 while(($row=mysqli_fetch_assoc($rs))) {
     $texto = $row['paso'];
     $id_procedimiento = $row ['id_procedimiento'];
@@ -95,6 +97,65 @@ file_put_contents('audio/nombre'.$id_receta.'.mp3', $audioContent);
 echo 'Audio content written to '.$id_receta.'.mp3' . PHP_EOL;
 
 $query= "UPDATE receta SET audio = 'audio/nombre$id_receta.mp3' WHERE id_receta = $id_receta";
+mysqli_query ($conexion, $query) OR DIE ("Error: ".mysqli_error($conexion));
+}
+
+// return $audioContent;
+
+//GUARDA AUDIO DE
+
+// instantiates a client
+$client = new TextToSpeechClient();
+$query=("SELECT * FROM datos_receta WHERE id_receta=17");
+$rs=mysqli_query($conexion, $query);
+while ($row=mysqli_fetch_array($rs)){
+    $id_datos=$row['id_datos'];
+}
+
+
+$query= ("SELECT porciones, cantidad, medida, ingrediente.nombre 
+        FROM datos_receta, ingrediente WHERE id_receta=$id_receta AND datos_receta.id_ingrediente 
+        = ingrediente.id_ingrediente");
+        $rs = mysqli_query ($conexion, $query);
+
+    while(($row=mysqli_fetch_assoc($rs))){ 
+        $porciones=$row ['porciones'];
+        $cantidad=$row['cantidad'];
+        $medida=$row['medida'];
+        $ingrediente=$row['nombre'];
+        $texto=$cantidad.$medida.$ingrediente;
+
+
+// sets text to be synthesised
+$synthesisInputText = (new SynthesisInput())
+
+    ->setText($texto);
+
+// build the voice request, select the language code ("en-US") and the ssml
+// voice gender
+$voice = (new VoiceSelectionParams())
+    ->setLanguageCode('es-ES')
+    ->setSsmlGender(SsmlVoiceGender::FEMALE);
+
+// Effects profile
+$effectsProfileId = "telephony-class-application";
+
+// select the type of audio file you want returned
+$audioConfig = (new AudioConfig())
+    ->setAudioEncoding(AudioEncoding::MP3)
+    ->setEffectsProfileId(array($effectsProfileId));
+
+// perform text-to-speech request on the text input with selected voice
+// parameters and audio file type
+$response = $client->synthesizeSpeech($synthesisInputText, $voice, $audioConfig);
+$audioContent = $response->getAudioContent();
+
+// the response's audioContent is binary
+file_put_contents('audio/datos'.$id_datos.'.mp3', $audioContent);
+
+echo 'Audio content written to '.$id_datos.'.mp3' . PHP_EOL;
+
+$query= "UPDATE datos_receta SET audio = 'audio/datos$id_datos.mp3' WHERE id_datos = $id_datos";
 mysqli_query ($conexion, $query) OR DIE ("Error: ".mysqli_error($conexion));
 }
 
