8deaeab55bb57e8cd4479af2b06c6fcab23bd70d
Dario <dario_gow3@hotmail.com>
2019-06-04 04:34:48 -0500

    muchas mas correcciones
    cronómetros agregados a subir receta js.
    autocompletado 100% funcional

--- subir_receta.php
+++ subir_receta.php
@@ -91,18 +91,18 @@
 
                     <div class="form-group col-md-2 text-center">
                         <select class="form-control custom-select" name="medida_1" required>
-                            <option value="Kg">Kg</option>
-                            <option value="g">g</option>
-                            <option value="mg">mg</option>
-                            <option value="Oz">Oz</option>
-                            <option value="L">L</option>
-                            <option value="ml">ml</option>
-                            <option value="Cucharada">Cucharada(s)</option>
-                            <option value="Cucharadita">Cucharadita(s)</option>
-                            <option value="Taza">Taza(s)</option>
-                            <option value="Pugno">Puño(s)</option>
-                            <option value="Pieza">Pieza(s)</option>
-                            <option value="x">Al gusto</option>
+                            <option value="Kilo gramo">Kg</option>
+                            <option value="gramo">g</option>
+                            <option value="mili gramo">mg</option>
+                            <option value="Onza">Oz</option>
+                            <option value="Litro">L</option>
+                            <option value="mili litro">ml</option>
+                            <option value="Cucharadas">Cucharada(s)</option>
+                            <option value="Cucharaditas">Cucharadita(s)</option>
+                            <option value="Tazas">Taza(s)</option>
+                            <option value="Puños">Puño(s)</option>
+                            <option value="Piezas">Pieza(s)</option>
+                            <option value="al gusto">Al gusto</option>
                         </select>
                     </div>
 
@@ -152,7 +152,7 @@
                 </div> 
             </div>
 
-            <div class="form-row" >
+            <div class="form-row">
                 <div class="form-group col-md-6 error alert alert-danger error_paso"></div>
             </div>
             
@@ -161,8 +161,40 @@
                     <input type="button" id="agregar_paso" name="agregar_paso" value="Agregar paso" class="btn boton_generico">
                 </div>
             </div>
+            <hr>
+
+            <p class="subtitulo_subir">Cronómetros</p>
+
+            <div id="crnm_group">
+                <div id="cronometro_1">
+                    <div class="form-row">
+                        <div class="form-group col-md-2 text-center">
+                            <input type="text" id="nombre_crnm_1" placeholder="nombre" name="nombre_crnm_1">
+                        </div>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group col-md-2 text-center">
+                            <input type="number" id="horas_1" min="1" max="60" placeholder="horas" name="horas_1">
+                        </div>
+                        <div class="form-group col-md-2 text-center">
+                            <input type="number" id="minutos_1" min="1" max="60" placeholder="minutos" name="minutos_1">
+                        </div>
+                        <div class="form-group col-md-2 text-center">
+                            <input type="number" id="segundos_1" min="1" max="60" placeholder="segundos" name="segundos_1">
+                        </div>
+                    </div>                
+                </div>
+                <hr>
+            </div>
             
-        
+
+            <div class="form-row">
+                <div class="form-group col-md-1 text-center">
+                    <input type="button" id="agregar_crnm" name="agregar_crnm" value="Agregar cronómetro" class="btn boton_generico">
+                </div>
+            </div>
+
             <hr>
 
             <div class="div_boton">
@@ -175,7 +207,14 @@
         <script src="js/bootstrap.min.js"></script>
         <script src="js/popper.min.js"></script>    
         <script src ="js/subir_receta.js"></script>
-        <script>
+        <script type="text/javascript">
+
+            let cont_name_ingr = 2;
+            let cont_ingr = 1;
+
+            $("#agregar_ingr").on("click", function(){
+                clonar_ingr();
+            });
 
             var ingredientes = <?= json_encode($ingredientes)?>;
             console.log(ingredientes);
@@ -184,14 +223,105 @@
                     source: ingredientes,
                     response: function(event, ui){
                         if (ui.content.length === 0) {
-                        $(this).css("border", "1px solid red");
-                    }else{
-                        $(this).css("border", "1px solid gray");
+                            $(this).css("border", "1px solid red");
+                        }else{
+                            $(this).css("border", "1px solid gray");
+                        }
                     }
-                }
                 });
             });
-        
+
+            function clonar_ingr(){
+    
+                let id_cant = "cant_"+cont_name_ingr;
+                let id_medida = "medida_"+cont_name_ingr;
+                let name_ingr = "ingr_"+cont_name_ingr;
+                let id_group = "ingrediente_"+cont_name_ingr;
+                let id_ingr = "name_ingr_"+cont_name_ingr;
+
+                var ingr_input = document.getElementById("ingr_group");
+
+                //contenedor de inputs de agregar ingrediente
+                var new_container = document.createElement("div");
+                new_container.setAttribute("class", "form-row");
+                new_container.setAttribute("id", id_group); 
+                ingr_input.appendChild(new_container);
+
+                //nuevo input text para insertar la cantidad del ingrediente
+                var div_cant = document.createElement("div")
+                div_cant.setAttribute("class", "form-group col-md-1 text-center");
+                new_container.appendChild(div_cant);
+                var new_cant = document.createElement("input");
+                new_cant.setAttribute("type", "number");
+                new_cant.setAttribute("name", id_cant);
+                new_cant.setAttribute("class", "cantidad");
+                new_cant.setAttribute("min", "0");
+                new_cant.setAttribute("value", "0.00");
+                new_cant.setAttribute("step", "0.01")
+                new_cant.setAttribute("required", "");
+                div_cant.appendChild(new_cant);
+
+                //clon del select para insertar la medida del ingrediente
+                var div_medida = document.createElement("div")
+                div_medida.setAttribute("class", "form-group col-md-2 text-center");
+                new_container.appendChild(div_medida);
+                var select_medida = document.querySelector("select[name = 'medida_1']");
+                var new_medida = select_medida.cloneNode(true);
+                new_medida.removeAttribute("name");
+                new_medida.setAttribute("name", id_medida);
+                div_medida.appendChild(new_medida);
+
+                //nuevo input text para insertar el nombre del ingrediente
+                var div_ingr = document.createElement("div");
+                div_ingr.setAttribute("class", "form-group col-md-3 text-center");
+                new_container.appendChild(div_ingr);
+                var new_ingr = document.createElement("input")
+                new_ingr.setAttribute("type", "text");
+                new_ingr.setAttribute("name", name_ingr);
+                new_ingr.setAttribute("class", "ingr ui-autocomplete-input");
+                new_ingr.setAttribute("id", id_ingr);
+                new_ingr.setAttribute("placeholder", "Nombre del ingrediente");
+                new_ingr.setAttribute("required", "");
+                div_ingr.appendChild(new_ingr);
+                $("#"+id_ingr).on("focus", function(){
+                        $(this).autocomplete({
+                        source: ingredientes,
+                        response: function(event, ui){
+                            if (ui.content.length === 0) {
+                                $(this).css("border", "1px solid red");
+                            }else{
+                                $(this).css("border", "1px solid gray");
+                            }
+                        }
+                    });
+                });
+                
+                //boton para eliminar ingrediente
+                var div_btn_delete = document.createElement("div");
+                div_btn_delete.setAttribute("class", "form-group col-md-1");
+                div_btn_delete.setAttribute("id", "delete_ingr");
+                new_container.appendChild(div_btn_delete);
+                var btn_delete = document.createElement("button");
+                btn_delete.setAttribute("type", "button");
+                btn_delete.setAttribute("class", "close");
+                btn_delete.setAttribute("aria-label", "Close");
+                btn_delete.setAttribute("id", "delete_ingr")
+                div_btn_delete.appendChild(btn_delete);
+                var span = document.createElement("span");
+                span.setAttribute("aria-hidden", "true");
+                span.innerHTML = "&times;"
+                btn_delete.appendChild(span);
+
+                btn_delete.addEventListener("click", function(){
+                    var deleted = document.querySelectorAll('div#ingr_group div.form-row');
+                    deleted[1].parentNode.removeChild(deleted[1]);
+                    cont_ingr-=1;
+                    cont_name_ingr-=1;
+                });
+
+                cont_ingr+=1;
+                cont_name_ingr+=1;
+            }
         </script>
 
     </body>
